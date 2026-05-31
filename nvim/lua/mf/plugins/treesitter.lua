575fe90fee7a50aa8694474c0b826abc34856908
majafranz <majafranz99@gmail.com>
2026-05-31 12:13:07 +0200

    adapt treesitter conf for 0.12

--- nvim/lua/mf/plugins/treesitter.lua
+++ nvim/lua/mf/plugins/treesitter.lua
@@ -1,32 +1,29 @@
-local function config()
-    require('nvim-treesitter').setup {
-        auto_install = false,
-        ensure_installed = {
-            'gitcommit',
-            'diff',
-            'git_rebase',
-            'lua',
-            'c',
-            'python',
-            'cpp',
-            'make',
-            'bash',
-            'markdown',
-            'markdown_inline',
-        },
-        -- A list of parser names
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false
-        },
-  }
-  vim.opt.foldmethod="expr"
-  vim.opt.foldexpr="nvim_treesitter#foldexpr()"
-  vim.opt.foldlevel=10
-end
+local languages = {
+    "gitcommit",
+    "diff",
+    "git_rebase",
+    "lua",
+    "c",
+    "python",
+    "cpp",
+    "make",
+    "bash",
+    "markdown",
+    "markdown_inline",
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = languages,
+    callback = function()
+        vim.treesitter.start()
+    end,
+})
 
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = config,
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false, -- plugin does not support lazy-loading.
+    build = ":TSUpdate",
+    init = function()
+        require("nvim-treesitter").install(languages)
+    end,
 }
